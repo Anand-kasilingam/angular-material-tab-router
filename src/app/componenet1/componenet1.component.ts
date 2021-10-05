01d72dd8ad08635659ed8b2e936852831393add8
Anand-kasilingam <56032458+Anand-kasilingam@users.noreply.github.com>
2021-10-05 22:17:55 +0530

    Initial commit

---
+++ src/app/componenet1/componenet1.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componenet1',
+  templateUrl: './componenet1.component.html',
+  styleUrls: ['./componenet1.component.css']
+})
+export class Componenet1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("first component loaded");
+    
+  }
+
+}
---
+++ src/app/componenet2/componenet2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componenet2',
+  templateUrl: './componenet2.component.html',
+  styleUrls: ['./componenet2.component.css']
+})
+export class Componenet2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("second component loaded");
+    
+  }
+
+}
